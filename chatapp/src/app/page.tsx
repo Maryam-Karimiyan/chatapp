a0842a49a36d731811181453c72a6a0964f7b94c
maryam <Ofogh.maryam14@gmail.com>
2025-09-03 16:27:39 +0330

    make some routes public or protected and give loggedin user data

--- chatapp/src/app/page.tsx
+++ chatapp/src/app/page.tsx
@@ -1,5 +1,15 @@
-
-
+"use client";
+import { UserButton, useUser } from "@clerk/nextjs";
 export default function Home() {
-  return <SignIn />
+  const { user } = useUser();
+  return (
+    <div className="p-10">
+      <UserButton />
+      <div className="flex flex-col gap-3 text-3xl">
+        <span>FirstName:{user?.firstName}</span>
+        <span>LastName:{user?.lastName}</span>
+        <span>UserName:{user?.username}</span>
+      </div>
+    </div>
+  );
 }
